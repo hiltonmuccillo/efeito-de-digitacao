d51b6e807b2d166cd90ba29436bb55f3f2897879
Hilton Muccillo <hmuccillo@gmail.com>
2023-11-08 20:47:54 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+(function(){
+
+  var typedItem = document.querySelector('#typed');
+
+  setInterval(function typed(){
+
+    // item 1
+    setTimeout (function() {
+      typedItem.innerHTML = "<span class='typed2'></span>";                        
+      typedItem.innerHTML = "<span class='typed1'>Já <span>atuei</span> no Grupo TV1.</span><span class='cursor-blink'></span>";
+      document.querySelector('#typed > .typed1 > span').className = "green";                        
+    }, 0)                                           
+    setTimeout (function() {
+        typedItem.innerHTML = "<span class='typed1'></span>";                        
+        typedItem.innerHTML = "<span class='typed2'>Já <span>atuei</span> no Grupo TV1.</span><span class='cursor-blink'></span>";                        
+        document.querySelector('#typed > .typed2 > span').className = "green";                        
+    }, 5000)            
+    setTimeout (function() {
+        typedItem.innerHTML = "<span class='typed2'></span>";
+    }, 5900)
+
+    // item 2
+    setTimeout (function() {
+      typedItem.innerHTML = "<span class='typed2'></span>";                        
+      typedItem.innerHTML = "<span class='typed1'>Já <span>atuei</span> na Wunderman Brasil.</span><span class='cursor-blink'></span>";
+      document.querySelector('#typed > .typed1 > span').className = "green";
+    }, 6000)            
+    setTimeout (function() {
+        typedItem.innerHTML = "<span class='typed1'></span>";                        
+        typedItem.innerHTML = "<span class='typed2'>Já <span>atuei</span> na Wunderman Brasil.</span><span class='cursor-blink'></span>";                        
+        document.querySelector('#typed > .typed2 > span').className = "green";
+    }, 11000)            
+    setTimeout (function() {
+        typedItem.innerHTML = "<span class='typed2'></span>";                                                        
+    }, 11900)
+
+    // item 3
+    setTimeout (function() {
+      typedItem.innerHTML = "<span class='typed2'></span>";
+      typedItem.innerHTML = "<span class='typed1'>Já <span>atuei</span> na Cheil Brasil.</span><span class='cursor-blink'></span>";
+       document.querySelector('#typed > .typed1 > span').className = "green";
+    }, 12000)            
+    setTimeout (function() {
+        typedItem.innerHTML = "<span class='typed1'></span>";
+      typedItem.innerHTML = "<span class='typed2'>Já <span >atuei</span> na Cheil Brasil.</span><span class='cursor-blink'></span>";              
+      document.querySelector('#typed > .typed2 > span').className = "green";
+    }, 17000)            
+    setTimeout (function() {
+        typedItem.innerHTML = "<span class='typed2'></span>";
+    }, 17900)
+    return typed;
+  }(), 18000);
+
+})();
